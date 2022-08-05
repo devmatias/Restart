b0d1e50369fa70c0d61d7c13bed010f225f4c355
devmatias <devmatias@gmail.com>
2022-08-05 14:30:59 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Matias Correa Franco de Faria on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Matias Correa Franco de Faria on 01/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnBoardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
